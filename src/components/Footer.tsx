55d061d2e4ca9eef6d02b9faa0f822aced2f5da0
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-08-06 13:19:48 -0700

    lint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Container, Box, Typography, Divider, Grid } from '@material-ui/core'
-import { Link } from 'react-router-dom'
-import { ReactComponent as C4CSvg } from '../svg/C4C.svg'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Box, Typography, Divider, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { ReactComponent as C4CSvg } from '../svg/C4C.svg';
 /*
 Template for future page components.
 Use MUI's CSS-in-JS makeStyles() as shown below for more complex CSS/
@@ -12,48 +12,48 @@ Every page must have a Helmet tag for SEO purposes.
 
 const useStyles = makeStyles({
   example: {
-    color: 'red'
+    color: 'red',
   },
   logo: {
     height: '3em',
     padding: '1em',
-    margin: 'auto'
+    margin: 'auto',
   },
   footerLink: {
-    color: 'inherit'
+    color: 'inherit',
   },
   location: {
-    fontWeight: 300
-  }
-})
+    fontWeight: 300,
+  },
+});
 
 const Footer: React.FC = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
-    <Container maxWidth='md'>
+    <Container maxWidth="md">
       <Divider />
 
-      <Box minHeight='10em' paddingY={5}>
+      <Box minHeight="10em" paddingY={5}>
         <Grid container spacing={1}>
           <Grid item xs={6}>
-            <Typography gutterBottom color='textSecondary' variant='subtitle2'>
-              <Link to='/apply' className={classes.footerLink}>
+            <Typography gutterBottom color="textSecondary" variant="subtitle2">
+              <Link to="/apply" className={classes.footerLink}>
                 Apply
               </Link>
               {' | '}
-              <Link to='/jumpstart' className={classes.footerLink}>
+              <Link to="/jumpstart" className={classes.footerLink}>
                 Jumpstart
               </Link>
               {' | '}
-              <Link to='/faq' className={classes.footerLink}>
+              <Link to="/faq" className={classes.footerLink}>
                 FAQ
               </Link>
             </Typography>
             <Typography
               gutterBottom
-              color='textSecondary'
-              align='left'
-              variant='subtitle2'
+              color="textSecondary"
+              align="left"
+              variant="subtitle2"
             >
               Contact us at c4cneu@gmail.com
             </Typography>
@@ -62,37 +62,37 @@ const Footer: React.FC = () => {
           <Grid item xs={6}>
             <Typography
               gutterBottom
-              color='textSecondary'
-              align='right'
-              variant='subtitle2'
+              color="textSecondary"
+              align="right"
+              variant="subtitle2"
             >
               <a
-                href='https://github.com/Code-4-Community'
+                href="https://github.com/Code-4-Community"
                 className={classes.footerLink}
               >
                 Github
               </a>
               {' | '}
-              <a href='https://c4cneu.slack.com' className={classes.footerLink}>
+              <a href="https://c4cneu.slack.com" className={classes.footerLink}>
                 Slack
               </a>
               {' | '}
               <a
-                href='https://www.facebook.com/c4cneu/'
+                href="https://www.facebook.com/c4cneu/"
                 className={classes.footerLink}
               >
                 Facebook
               </a>
               {' | '}
               <a
-                href='http://instagram.com/c4cneu'
+                href="http://instagram.com/c4cneu"
                 className={classes.footerLink}
               >
                 Instagram
               </a>
               {' | '}
               <a
-                href='https://www.linkedin.com/company/code-4-community/'
+                href="https://www.linkedin.com/company/code-4-community/"
                 className={classes.footerLink}
               >
                 LinkedIn
@@ -100,13 +100,13 @@ const Footer: React.FC = () => {
             </Typography>
             <Typography
               gutterBottom
-              color='textSecondary'
-              align='right'
-              variant='subtitle2'
+              color="textSecondary"
+              align="right"
+              variant="subtitle2"
             >
               Join our{' '}
               <a
-                href='https://c4cneu.us4.list-manage.com/subscribe?u=4b534cd2e8fe8be8150d03977&id=b1915b8b8b'
+                href="https://c4cneu.us4.list-manage.com/subscribe?u=4b534cd2e8fe8be8150d03977&id=b1915b8b8b"
                 className={classes.footerLink}
               >
                 mailing list
@@ -115,19 +115,19 @@ const Footer: React.FC = () => {
             </Typography>
           </Grid>
           <Grid item xs>
-            <Box display='flex' flexDirection='column' alignItems='center'>
+            <Box display="flex" flexDirection="column" alignItems="center">
               <C4CSvg className={classes.logo} />
               <Typography
-                color='textSecondary'
-                align='center'
-                variant='subtitle2'
+                color="textSecondary"
+                align="center"
+                variant="subtitle2"
               >
                 Made with love by Code4Community.
               </Typography>
               <Typography
-                color='textSecondary'
-                align='center'
-                variant='subtitle2'
+                color="textSecondary"
+                align="center"
+                variant="subtitle2"
                 className={classes.location}
               >
                 Northeastern University, Boston MA
@@ -137,7 +137,7 @@ const Footer: React.FC = () => {
         </Grid>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
