c1b6a78055259dd77e5ee0e55309af60cf71c84d
Jack Blanc <jblanc222@gmail.com>
2020-08-05 23:42:10 -0400

    creating jumpstart page + accordion component

--- src/components/Jumpstart.tsx
+++ src/components/Jumpstart.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Box, Paper } from '@material-ui/core';
-
-/*
-Template for future page components.
-Use MUI's CSS-in-JS makeStyles() as shown below for more complex CSS/
-Otherwise, use <Box> components where possible for inline styling.
-Every page must have a Helmet tag for SEO purposes.
-*/
+import React from "react";
+import { Helmet } from "react-helmet";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Box,
+  Paper,
+  Grid,
+  Typography,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Hero from "./Hero";
+import { ReactComponent as JumpstartSvg } from "../svg/jumpstart.svg";
+import JumpstartAccordion from "./JumpstartAccordion";
 
 const useStyles = makeStyles({
-  example: {
-    color: 'red',
+  section: {
+    marginTop: "10vh",
+    marginBottom: "10vh",
+  },
+  subtext: {
+    marginTop: "1.5vh",
+  },
+  flexSection: {
+    marginTop: "10vh",
+    marginBottom: "10vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    width: "40vh",
+    height: "10vh",
+    fontSize: "48px",
+  },
+  accordionContainer: {
+    display: "flex",
+    flexDirection: "column",
   },
 });
 
@@ -24,8 +51,123 @@ const Template: React.FC = () => {
         <title>Write Title Here</title>
         <meta name="description" content="Write a description for SEO here" />
       </Helmet>
-      <Box my={4}>
-        <Paper className={classes.example} />
+      <Hero
+        subtitle="Our brand new full-stack development program for first time developers."
+        title="Jumpstart"
+        SvgNode={JumpstartSvg}
+      />
+
+      <Grid
+        container
+        justify="space-between"
+        alignItems="flex-start"
+        direction="row"
+        className={classes.section}
+        spacing={6}
+      >
+        <Grid item md={5}>
+          <Typography variant="h4">What is Jumpstart?</Typography>
+          <Typography variant="body1" className={classes.subtext}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            vitae eros laoreet nisl ornare dignissim at sit amet leo. Integer
+            convallis malesuada elit, fringilla venenatis eros facilisis ut. Nam
+            ut accumsan mauris. Aenean at odio dolor. In ultricies ipsum risus,
+            nec porttitor turpis ornare in.
+          </Typography>
+        </Grid>
+        <Grid item md={5}>
+          <Typography variant="h4">What will I learn?</Typography>
+          <Typography variant="body1" className={classes.subtext}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            vitae eros laoreet nisl ornare dignissim at sit amet leo. Integer
+            convallis malesuada elit, fringilla venenatis eros facilisis ut. Nam
+            ut accumsan mauris. Aenean at odio dolor. In ultricies ipsum risus,
+            nec porttitor turpis ornare in.
+          </Typography>
+        </Grid>
+        <Grid item md={5}>
+          <Typography variant="h4">Can I participate?</Typography>
+          <Typography variant="body1" className={classes.subtext}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            vitae eros laoreet nisl ornare dignissim at sit amet leo. Integer
+            convallis malesuada elit, fringilla venenatis eros facilisis ut. Nam
+            ut accumsan mauris. Aenean at odio dolor. In ultricies ipsum risus,
+            nec porttitor turpis ornare in.
+          </Typography>
+        </Grid>
+        <Grid item md={5}>
+          <Typography variant="h4">Something something</Typography>
+          <Typography variant="body1" className={classes.subtext}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            vitae eros laoreet nisl ornare dignissim at sit amet leo. Integer
+            convallis malesuada elit, fringilla venenatis eros facilisis ut. Nam
+            ut accumsan mauris. Aenean at odio dolor. In ultricies ipsum risus,
+            nec porttitor turpis ornare in.
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Box className={classes.flexSection}>
+        <Button variant="contained" color="primary" className={classes.button}>
+          Sign Me Up!
+        </Button>
+      </Box>
+
+      <Box className={classes.section}>
+        <JumpstartAccordion
+          sections={[
+            {
+              title: "Week 1",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 2",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 3",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 4",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 5",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 6",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 7",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 8",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 9",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+            {
+              title: "Week 10",
+              body:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+            },
+          ]}
+        />
       </Box>
     </Container>
   );
