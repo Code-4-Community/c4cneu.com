f1549933772973291d1f4ff7b63a7f5ded0d1d92
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-08-06 20:01:23 -0700

    lazy loading routes

--- src/components/Apply.tsx
+++ src/components/Apply.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { Container, Box } from '@material-ui/core'
-import ApplyGrid from './ApplyGrid'
-import Hero from './Hero'
-import { ReactComponent as SVG } from '../svg/team.svg'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Container, Box } from '@material-ui/core';
+import ApplyGrid from './ApplyGrid';
+import Hero from './Hero';
+import { ReactComponent as SVG } from '../svg/team.svg';
 
 const Apply: React.FC = () => {
   React.useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <Container maxWidth='md'>
+    <Container maxWidth="md">
       <Helmet>
         <title>Apply</title>
         <meta
-          name='description'
-          content='Apply to join Code4Community and help create software for non-profits in Boston.'
+          name="description"
+          content="Apply to join Code4Community and help create software for non-profits in Boston."
         />
       </Helmet>
 
       <Hero
-        title='Open Positions'
-        subtitle='Join us and make a difference helping non-profits in need.'
+        title="Open Positions"
+        subtitle="Join us and make a difference helping non-profits in need."
         SvgNode={SVG}
       />
-      <Box  paddingBottom={'10em'}>
+      <Box paddingBottom={'10em'}>
         <ApplyGrid />
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Apply
+export default Apply;
