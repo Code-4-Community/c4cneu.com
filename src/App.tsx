a400e4f1a23c29493445abcca31cc27bf51f17b3
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-08-06 20:22:45 -0700

    lazy load images, eager load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+
+import Home from './components/Home';
+import Apply from './components/Apply';
+import ApplyDeveloper from './components/ApplyDeveloper';
+import ApplyProductDesigner from './components/ApplyProductDesigner';
+import ApplyBrandDesigner from './components/ApplyBrandDesigner';
+
+import Partner from './components/Partner';
+import Projects from './components/Projects';
+import Jumpstart from './components/Jumpstart';
+import Events from './components/Events';
+import Board from './components/Board';
+import FAQ from './components/FAQ';
+import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 import { ThemeProvider } from '@material-ui/core';
 
 import theme from './theme';
-
-const Home = lazy(() => import('./components/Home'));
-const Apply = lazy(() => import('./components/Apply'));
-const ApplyDeveloper = lazy(() => import('./components/ApplyDeveloper'));
-const ApplyProductDesigner = lazy(() =>
-  import('./components/ApplyProductDesigner')
-);
-const ApplyBrandDesigner = lazy(() =>
-  import('./components/ApplyBrandDesigner')
-);
-
-const Partner = lazy(() => import('./components/Partner'));
-const Projects = lazy(() => import('./components/Projects'));
-const Jumpstart = lazy(() => import('./components/Jumpstart'));
-const Events = lazy(() => import('./components/Events'));
-const Board = lazy(() => import('./components/Board'));
-const FAQ = lazy(() => import('./components/FAQ'));
-const NotFound = lazy(() => import('./components/NotFound'));
-const NavBar = lazy(() => import('./components/NavBar'));
-const Footer = lazy(() => import('./components/Footer'));
-
-const renderLoader = () => <></>;
-
 const App: React.FC = () => {
   return (
     <>
@@ -39,36 +32,34 @@ const App: React.FC = () => {
           />
         </Helmet>
 
-        <Suspense fallback={renderLoader()}>
-          <Router>
-            <NavBar />
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/apply" exact component={Apply} />
-              <Route path="/apply/developer" exact component={ApplyDeveloper} />
-              <Route
-                path="/apply/product-designer"
-                exact
-                component={ApplyProductDesigner}
-              />
-              <Route
-                path="/apply/brand-designer"
-                exact
-                component={ApplyBrandDesigner}
-              />
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/apply" exact component={Apply} />
+            <Route path="/apply/developer" exact component={ApplyDeveloper} />
+            <Route
+              path="/apply/product-designer"
+              exact
+              component={ApplyProductDesigner}
+            />
+            <Route
+              path="/apply/brand-designer"
+              exact
+              component={ApplyBrandDesigner}
+            />
 
-              <Route path="/partner" exact component={Partner} />
-              <Route path="/projects" exact component={Projects} />
-              <Route path="/jumpstart" exact component={Jumpstart} />
-              <Route path="/events" exact component={Events} />
-              <Route path="/board" exact component={Board} />
-              <Route path="/faq" exact component={FAQ} />
+            <Route path="/partner" exact component={Partner} />
+            <Route path="/projects" exact component={Projects} />
+            <Route path="/jumpstart" exact component={Jumpstart} />
+            <Route path="/events" exact component={Events} />
+            <Route path="/board" exact component={Board} />
+            <Route path="/faq" exact component={FAQ} />
 
-              <Route path="*" exact component={NotFound} />
-            </Switch>
-            <Footer />
-          </Router>
-        </Suspense>
+            <Route path="*" exact component={NotFound} />
+          </Switch>
+          <Footer />
+        </Router>
       </ThemeProvider>
     </>
   );
