95008daaadfff5e9d7fd207bb9e9546a3fc81a72
Kimin Lee <kiminlee2000225@gmail.com>
2020-08-09 10:44:58 -0400

    style fixes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, Grid } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import { ReactComponent as SVG } from '../svg/people.svg';
 
 import Member from './Member';
@@ -29,7 +29,7 @@ const useStyles = makeStyles({
   },
   hero: {
     marginBottom: 10,
-  }
+  },
 });
 
 const Template: React.FC = () => {
@@ -39,7 +39,10 @@ const Template: React.FC = () => {
       <Container maxWidth="md" className={classes.hero}>
         <Helmet>
           <title>E-BOARD</title>
-          <meta name="description" content="The Code4Community E-Boards page." />
+          <meta
+            name="description"
+            content="The Code4Community E-Boards page."
+          />
         </Helmet>
         <Hero
           title="E-Board"
@@ -50,48 +53,55 @@ const Template: React.FC = () => {
       <Container maxWidth="md">
         <div className={classes.root}>
           <Grid container justify="center" alignItems="center" spacing={3}>
-            <Member 
-              name="Liam Moynihan" 
-              position="President" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Liam-M.jpg" 
-              linkedin="https://www.linkedin.com/in/liam-moynihan-b75806123/" 
-              email="moynihan.li@northeastern.edu"/>
-            <Member 
-              name="Jack Blanc" 
-              position="Director of Engineering" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Jack-B.jpg" 
-              linkedin="https://www.linkedin.com/in/blancjack/" 
-              email="blanc.ja@northeastern.edu"/>
-            <Member 
-              name="Reine Nisheiwat" 
-              position="Director of Design" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Reine-N.jpg" 
-              linkedin="https://www.linkedin.com/in/reinenish/" 
-              email="nisheiwat.r@northeastern.edu"/>
-            <Member 
-              name="Sadaf Khansalar" 
-              position="Club Operations & Administration Director" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Sadaf-K.jpg" 
-              linkedin="https://www.linkedin.com/in/sadafkhansalar/" 
-              email="khansalar.s@northeastern.edu"/>
-            <Member 
-              name="Abby Emison" 
-              position="Recruitment & Community Engagement Director" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Abby-E.jpg" 
-              linkedin="https://www.linkedin.com/in/abigail-emison-26b448125/" 
-              email="emison.a@northeastern.edu"/>
-            <Member 
-              name="Jack Tonina" 
-              position="School Liaison" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Jack-T.jpg" 
-              linkedin="https://www.linkedin.com/in/jack-tonina-6328b9147/" 
-              email="tonina.j@northeastern.edu"/>
-            <Member 
-              name="Justin Konecny" 
-              position="Technical Lead" 
-              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Justin-K.jpg" 
-              linkedin="https://www.linkedin.com/in/justin-konecny/" 
-              email="konecny.j@northeastern.edu"/>
+            <Member
+              name="Liam Moynihan"
+              position="President"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Liam-M.jpg"
+              linkedin="https://www.linkedin.com/in/liam-moynihan-b75806123/"
+              email="moynihan.li@northeastern.edu"
+            />
+            <Member
+              name="Jack Blanc"
+              position="Director of Engineering"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Jack-B.jpg"
+              linkedin="https://www.linkedin.com/in/blancjack/"
+              email="blanc.ja@northeastern.edu"
+            />
+            <Member
+              name="Reine Nisheiwat"
+              position="Director of Design"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Reine-N.jpg"
+              linkedin="https://www.linkedin.com/in/reinenish/"
+              email="nisheiwat.r@northeastern.edu"
+            />
+            <Member
+              name="Sadaf Khansalar"
+              position="Club Operations & Administration Director"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Sadaf-K.jpg"
+              linkedin="https://www.linkedin.com/in/sadafkhansalar/"
+              email="khansalar.s@northeastern.edu"
+            />
+            <Member
+              name="Abby Emison"
+              position="Recruitment & Community Engagement Director"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Abby-E.jpg"
+              linkedin="https://www.linkedin.com/in/abigail-emison-26b448125/"
+              email="emison.a@northeastern.edu"
+            />
+            <Member
+              name="Jack Tonina"
+              position="School Liaison"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Jack-T.jpg"
+              linkedin="https://www.linkedin.com/in/jack-tonina-6328b9147/"
+              email="tonina.j@northeastern.edu"
+            />
+            <Member
+              name="Justin Konecny"
+              position="Technical Lead"
+              image="https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Justin-K.jpg"
+              linkedin="https://www.linkedin.com/in/justin-konecny/"
+              email="konecny.j@northeastern.edu"
+            />
           </Grid>
         </div>
       </Container>
