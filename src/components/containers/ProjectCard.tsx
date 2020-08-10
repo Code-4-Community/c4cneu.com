6ef925048dc142806aa28af7d425d582fd39f233
Brandon Liang <54skyxenon@gmail.com>
2020-08-10 14:16:18 -0700

    Made project cards a component

---
+++ src/components/containers/ProjectCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {
+    CardActionArea,
+    CardContent,
+    Button,
+    Typography,
+    CardMedia,
+    Card,
+    CardActions,
+    makeStyles
+}
+    from '@material-ui/core';
+
+const useStyles = makeStyles({
+    'project-card': {},
+    'project-img': {}
+});
+
+interface CardProps {
+    readonly title: string;
+    readonly paragraph: string;
+    readonly path: string;
+    readonly abbr: string;
+}
+
+const ProjectCard: React.FC<CardProps> = ({ title, paragraph, path, abbr }) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes['project-card']}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    className={classes['project-img']}
+                    image={path}
+                    alt={path}
+                    title={abbr}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {paragraph}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+        </Button>
+                <Button size="small" color="primary">
+                    Learn More
+        </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default ProjectCard;
--- src/components/containers/Projects.tsx
+++ src/components/containers/Projects.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Container, Box } from '@material-ui/core';
 
-import {
-  CardActionArea,
-  CardContent,
-  Button,
-  Typography,
-  CardMedia,
-  Card,
-  Container,
-  Box,
-  CardActions,
-  makeStyles
-}
-  from '@material-ui/core';
-
-const useStyles = makeStyles({
-  example: {
-    color: 'red',
-  },
-  'project-card': {},
-  'project-img': {}
-});
+import ProjectCard from './ProjectCard';
 
 const Projects: React.FC = () => {
-  const classes = useStyles();
   return (
     <Container maxWidth="md">
       <Helmet>
@@ -32,72 +12,23 @@ const Projects: React.FC = () => {
         <meta name="description" content="C4C delivers web applications to Boston-based nonprofits." />
       </Helmet>
       <Box my={4}>
+        <ProjectCard title={"Lucy's Love Bus"} paragraph={"Lucy’s Love Bus improves quality of life for children with cancer \
+                        or other life-threatening illnesses with integrative therapies like massage, meditation, \
+                        acupuncture, music therapy, and therapeutic horseback riding. Integrative therapies ease children’s \
+                        pain and anxiety during and after traditional medical treatments. \
+                        The Sajni Center is a space of hope and healing for children with life-threatening illness or chronic \
+                        medical conditions and their families. Here they host events for the children and their families. We created \
+                        the event planning and ticket purchasing system for all the events hosted by Lucy's Love Bus."}
+          path="./llb.png"
+          abbr="LLB" />
 
-        <Card className={classes['project-card']}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              className={classes['project-img']}
-              image="./llb.png"
-              alt="llb.png"
-              title="LLB"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Lucy's Love Bus
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lucy’s Love Bus improves quality of life for children with cancer or other life-threatening illnesses with
-                integrative therapies like massage, meditation, acupuncture, music therapy, and therapeutic horseback riding.
-                Integrative therapies ease children’s pain and anxiety during and after traditional medical treatments.
-                The Sajni Center is a space of hope and healing for children with life-threatening illness or chronic
-                medical conditions and their families. Here they host events for the children and their families. We created
-                the event planning and ticket purchasing system for all the events hosted by Lucy's Love Bus.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-        </Button>
-            <Button size="small" color="primary">
-              Learn More
-        </Button>
-          </CardActions>
-        </Card>
-
-        <Card className={classes['project-card']}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              className={classes['project-img']}
-              image="./sfft.png"
-              alt="sfft.png"
-              title="SFFT"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Speak For The Trees
-          </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Speak for the Trees Boston aims to improve the size and health of the urban forest in the greater Boston area,
-                with a focus on under-served and under-canopied neighborhoods. They work with volunteers to inventory (collect data)
-                trees, plant trees, and educate those about trees. C4C built a block reservation system, allowing SFFT's volunteers
-                to reserve blocks of trees that they will be able to inventory and then mark them as complete when they are
-                finished collecting the required data.
-          </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-        </Button>
-            <Button size="small" color="primary">
-              Learn More
-        </Button>
-          </CardActions>
-        </Card>
-
+        <ProjectCard title={"Speak For The Trees"} paragraph={"Speak for the Trees Boston aims to improve the size and health \
+                of the urban forest in the greater Boston area, with a focus on under-served and under-canopied neighborhoods. \
+                They work with volunteers to inventory (collect data) trees, plant trees, and educate those about trees. \
+                C4C built a block reservation system, allowing SFFT's volunteers to reserve blocks of trees that they will \
+                be able to inventory and then mark them as complete when they are finished collecting the required data."}
+          path="./sfft.png"
+          abbr="SFFT" />
       </Box>
     </Container>
   );
