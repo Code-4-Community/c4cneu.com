45a60090fb28e3bd7e178f8d60e0cbf23da9b44f
Max Sebso <maxsebso@gmail.com>
2020-08-30 11:37:39 -0400

    Moved data to a JSON format in a seperate file

--- src/pages/jumpstart/Jumpstart.tsx
+++ src/pages/jumpstart/Jumpstart.tsx
@@ -5,6 +5,7 @@ import { Container, Box, Grid, Typography, Button } from '@material-ui/core';
 import Hero from '../../components/Hero';
 import { ReactComponent as JumpstartSvg } from './jumpstart.svg';
 import JumpstartAccordion from '../../components/Accordion';
+import JumpstartData from './JumpstartData';
 // @ts-ignore
 import Fade from 'react-reveal/Fade';
 
@@ -150,51 +151,7 @@ const Jumpstart: React.FC = () => {
         <Box paddingBottom={5}>
           <Container maxWidth="sm">
             <JumpstartAccordion
-              sections={[
-                {
-                  title:
-                    'Sunday Sept. 27th - Project Overview and Terminal Setup',
-                  body: `We'll be helping you set up the software you'll need to run the project and teaching you some basic terminal commands you'll need to be able to interact with your program.`,
-                },
-                {
-                  title: 'Sunday Oct. 4th - Git and Frontend Basics',
-                  body: `A quick tour of Git, one of the most useful tools for development, and the gist of HTML, CSS, and Javascript.`,
-                },
-                {
-                  title: 'Sunday Oct. 11th - React',
-                  body: `Yeah, that's right. We're going to be learning about that cool buzzword. (Trust us though, it makes frontend stuff easy and powerful).`,
-                },
-                {
-                  title:
-                    'Sunday Oct. 18th - Server, Requests, and Backend Basics',
-                  body: `What makes communication possible. We'll talk about the basic idea of servers and REST requests, and then get you working with back end routing.`,
-                },
-                {
-                  title:
-                    'Sunday Oct. 25th - Testing and Advanced Backend Techniques',
-                  body: `We know testing isn't as fun, but there are actually some pretty cool tools that you can use here. Also, we'll be going over other useful request types and ideas.`,
-                },
-                {
-                  title: 'Sunday Nov. 1st - Advanced Frontend Techniques',
-                  body: `Asynchronous frontend requests can sound scary, but we'll teach it in a way that shows that they really aren't (most of the time).`,
-                },
-                {
-                  title: 'Sunday Nov. 8th - Database Basics',
-                  body: `Do you want to know how to store things efficiently without keeping them just in memory? It's basically Excel, but better. And you can use it in code!`,
-                },
-                {
-                  title: 'Sunday Nov. 15th - Advanced Database Concepts',
-                  body: `Databases can get pretty complicated though, but it can help a lot in the long run.`,
-                },
-                {
-                  title: 'Sunday Nov. 22nd - Cloud Deployment and Hosting',
-                  body: `Are you brave enough to show your friends and family what you made?`,
-                },
-                {
-                  title: 'Sunday Dec. 6th - Project Wrap Up ',
-                  body: `Bye bye. We loved working with you. :')`,
-                },
-              ]}
+              sections={JumpstartJSON.jumpstartLayout}
             />
           </Container>
         </Box>
