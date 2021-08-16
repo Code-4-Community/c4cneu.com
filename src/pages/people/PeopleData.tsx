79116f7f96c4f83ff2c4e67a1aab436e6285bd85
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-16 19:23:17 -0400

    add daniel

--- src/pages/people/PeopleData.tsx
+++ src/pages/people/PeopleData.tsx
@@ -162,4 +162,11 @@ export const people: Person[] = [
     linkedin: 'https://www.linkedin.com/in/derekleung27/',
     email: 'dleung2140@gmail.com'
   },
+  {
+    name: 'Daniel Jung',
+    position: 'Developer',
+    image: 'https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2021/daniel_jung.jpg',
+    linkedin: 'https://www.linkedin.com/in/djung335/',
+    email: 'jung.d@northeastern.edu'
+  }
 ];
