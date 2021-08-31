a8ed873823f190f20c264c5f9baeecf9cecd4eac
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-31 11:39:09 -0400

    update apply page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-
-import Home from './pages/home/Home';
-import Apply from './pages/apply/Apply';
-import ApplyDeveloper from './pages/apply/developer/ApplyDeveloper';
-import ApplyProjectManager from './pages/apply/project-manager/ApplyProjectManager';
-import ApplyProductDesigner from './pages/apply/product-designer/ApplyProductDesigner';
-import ApplyBrandDesigner from './pages/apply/brand-designer/ApplyBrandDesigner';
-import Projects from './pages/projects/Projects';
-import Events from './pages/events/Events';
-import People from './pages/people/People';
-import FAQ from './pages/faq/FAQ';
-import ResumeBook from './pages/resumeBook/ResumeBook';
-import NotFound from './pages/notfound/NotFound';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-
 import { ThemeProvider } from '@material-ui/core';
-
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Apply from './pages/apply/Apply';
+import ApplyBrandDesigner from './pages/apply/brand-designer/ApplyBrandDesigner';
+import ApplyDeveloper from './pages/apply/developer/ApplyDeveloper';
+import ApplyProductDesigner from './pages/apply/product-designer/ApplyProductDesigner';
+import ApplyProjectManager from './pages/apply/project-manager/ApplyProjectManager';
+import Events from './pages/events/Events';
+import FAQ from './pages/faq/FAQ';
+import Home from './pages/home/Home';
+import NotFound from './pages/notfound/NotFound';
+import People from './pages/people/People';
+import Projects from './pages/projects/Projects';
+import ResumeBook from './pages/resumeBook/ResumeBook';
 import theme from './theme';
+
 const App: React.FC = () => {
   return (
     <>
