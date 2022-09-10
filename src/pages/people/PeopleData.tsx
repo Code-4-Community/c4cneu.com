c83865b1e79bf7f04971e1ecfc0f18fc03ce5f40
jennya alexeyeva <jennyaalexeyeva@gmail.com>
2022-09-10 16:23:14 -0400

    add self to PeopleData page

--- src/pages/people/PeopleData.tsx
+++ src/pages/people/PeopleData.tsx
@@ -279,4 +279,12 @@ export const people: Person[] = [
     linkedin: 'https://www.linkedin.com/in/srivastava-as/',
     email: 'srivastava.as@northeastern.edu',
   },
+  {
+    name: 'Jennya Alexeyeva',
+    position: 'Developer',
+    image:
+      'https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2020/Jennya-A.jpg',
+    linkedin: 'https://www.linkedin.com/in/jennya-alexeyeva/',
+    email: 'alexeyeva.e@northeastern.edu',
+  },
 ];
