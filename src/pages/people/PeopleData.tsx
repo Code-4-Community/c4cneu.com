cfe78f5f6454e524c6ffc4d62ea22bc2f1572b2a
Jenny <dolientrang@gmail.com>
2022-09-22 12:57:02 -0400

    update personal information

--- src/pages/people/PeopleData.tsx
+++ src/pages/people/PeopleData.tsx
@@ -301,18 +301,18 @@ export const people: Person[] = [
     email: 'sugumar.d@northeastern.edu'
   },
   {
-    name: 'Jenny Do',
-    position: 'Developer',
-    image: 'https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2022/Jenny+Do+pic+500x500px.png',
-    linkedin: 'https://www.linkedin.com/in/trang-do-461492169/',
-    email: 'do.tran@northeastern.edu',
-  },
-  {
     name: 'Avery Huang',
     position: 'Developer',
     image: 'https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2022/Avery_Huang.jpg',
     linkedin: 'https://www.linkedin.com/in/averyhuang0h/',
     email: 'huang.av@northeastern.edu'  
+  },
+  {
+    name: 'Jenny Do',
+    position: 'Developer',
+    image: 'https://c4cneu-public.s3.us-east-2.amazonaws.com/Board/2022/Jenny+Do+pic+500x500px.png',
+    linkedin: 'https://www.linkedin.com/in/trang-do-461492169/',
+    email: 'do.tran@northeastern.edu',
   },
   {
     name:'Ryan Kennedy',
